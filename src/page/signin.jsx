5be48f39e8d4f9faf7838a4b1dc3b251fa53d4e6
Kurasao <jamerolenor@gmail.com>
2024-10-26 23:05:28 +0800

    bery smol changes

--- src/page/signin.jsx
+++ src/page/signin.jsx
@@ -35,8 +35,8 @@ const SignIn = () => {
               </button>
             </div>
           </form>
-          <div className="mt-6">
-            <div className="text-left text-xs text-gray-700">Dont have an account?</div>
+          <div className="mt-2">
+            <div className="mb-2 text-left text-xs text-gray-700">Dont have an account?</div>
             <button className="w-full flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
               Create an Account
             </button>
