845de8ac366cbf7caaa6572cdd2bc951f630749d
Kurasao <jamerolenor@gmail.com>
2024-11-04 15:35:11 +0800

    added "heropage.sjx" "added new routes" 'fix sign up role route' "removed unused imports"

--- src/page/Client/home-page/home.jsx
+++ src/page/Client/home-page/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import NavBar from './../_components/navbar';
 import ProductSection from './product-section';
